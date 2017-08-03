ae3881721f0e205c0e4f75e314957b9f4503cb9b
Alexander Shagov <x48j35@gmail.com>
2017-08-03 13:52:22 +0300

    Add values and variables experiments

---
+++ values_and_variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Hello" + "World" + "!")
+  fmt.Println(12490/492.0)
+  fmt.Println(true)
+  fmt.Println(false)
+
+  var name string = "Alex"
+  surname := "Shagov"
+  var number int = 21
+  var boolean bool = true
+  var b byte = 255
+
+  fmt.Println(name)
+  fmt.Println(surname)
+  fmt.Println(number)
+  fmt.Println(boolean)
+  fmt.Println(b)
+}
